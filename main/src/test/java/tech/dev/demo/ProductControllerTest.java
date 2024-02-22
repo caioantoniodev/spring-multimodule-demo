2d070fbd631b0cb3594661f5f9ad69bffa4536c3
Caio Antonio <caio.antonio.c@outlook.com>
2024-02-22 08:51:55 -0300

    add controller test

--- main/src/test/java/tech/dev/demo/ProductControllerTest.java
+++ main/src/test/java/tech/dev/demo/ProductControllerTest.java
@@ -1,5 +1,7 @@
 package tech.dev.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -7,22 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import tech.dev.demo.Main;
 import tech.dev.demo.product.ProductController;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@ExtendWith(MockitoExtension.class)
 @WebMvcTest(ProductController.class)
 @ContextConfiguration(classes = Main.class)
 public class ProductControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void shouldReturnListOfProducts() throws Exception {
@@ -46,5 +50,17 @@ public class ProductControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void shouldRemoveProductsByNameAndReturnTrue() throws Exception {
+        var mvcResult = this.mvc.perform(delete("/products/iphone"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var response = this.objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Boolean.class);
+
+        Assertions.assertTrue(response);
     }
 }
